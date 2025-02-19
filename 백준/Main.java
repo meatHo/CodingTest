0a2838edd83dcda70ac7b8d02cb4e4b74a444e36
meatHo <kkiho19@gmail.com>
2025-02-20 01:26:04 +0900

    7576

--- 백준/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.io.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-    }
-}
