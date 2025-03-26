fc137f7eab88a66b74c64d91b6ac2b639ad83dae
meatHo <kkiho19@gmail.com>
2025-03-26 13:46:56 +0900

    3-26

--- 백준/5430중요.java
+++ 백준/5430중요.java
@@ -5,7 +5,7 @@ import java.io.*;
 //
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException { 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int num = Integer.parseInt(br.readLine());
